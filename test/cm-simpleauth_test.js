e97ac53ba8922fd0da10abfa45fe9f24714f6c67
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-06-23 22:16:53 +0200

    test

--- test/cm-simpleauth_test.js
+++ test/cm-simpleauth_test.js
@@ -14,29 +14,53 @@ chai.should();
 
 var simpleauth = require('../lib/cm-simpleauth.js');
 var EventEmitter = require('events').EventEmitter;
+
 var testHost = new EventEmitter();
+
 describe('cm-simpleauth module', function() {
     it('is defined', function() {
         simpleauth.should.be.a('function');
     });
 
     before(function(done) {
-        this.mw = simpleauth('test/users.json',done);
+        this.mw = simpleauth('test/users.json', done);
     });
 
     it('return a middleware', function() {
         this.mw.should.be.a('function');
     });
 
-    it('authenticate test user', function(done) {
-        this.mw(testHost);
-        testHost.emit('authorizeUser', null, 'ola', 'parroit', function(err, success) {
-            if (err) {
-                return done(err);
-            }
-            success.should.be.true;
-            done();
+    describe('when mw is used on a connect-mail app instance', function() {
+        before(function(done) {
+            
+            this.mw(testHost);
+            testHost.emit('authorizeUser', null, 'ola', 'parroit', function(err, success) {
+                if (err) {
+                    return done(err);
+                }
+                this.success = success;
+                done();
+
+            }.bind(this));
+            
+        });
+
+        it('authenticate test user', function() {
+            this.success.should.be.equal(true);
+
+        });
+
+        it('save user in request', function() {
+            testHost.user.should.be.deep.equal({
+                username: 'ola',
+                password: 'parroit',
+                fullname: 'parroit',
+                email: 'parroit@ebansoftware.net',
+                status: 'parroit'
+            });
 
         });
     });
+
+
 });
